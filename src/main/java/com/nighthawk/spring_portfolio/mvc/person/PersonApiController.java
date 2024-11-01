f588ee94ce7d0707803760bb0d43ed5a67b5f269
shuban-789 <cybergeek11929@gmail.com>
2024-11-01 00:43:36 -0700

    person api controller parserror is fixed

--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonApiController.java
@@ -1,18 +1,28 @@
 package com.nighthawk.spring_portfolio.mvc.person;
 
-import org.apache.logging.log4j.core.parser.ParseException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import lombok.Getter;
-
-import java.util.*;
-import java.text.SimpleDateFormat;
 
 /**
  * This class provides RESTful API endpoints for managing Person entities.
