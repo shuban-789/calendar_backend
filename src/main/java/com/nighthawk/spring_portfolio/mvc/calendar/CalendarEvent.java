52e80f3b9caa4e150dd31ca75de6b6ee6e6c2d50
shuban-789 <cybergeek11929@gmail.com>
2024-11-01 00:26:22 -0700

    fixed the calendar view controller, made a compliant POJO

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/calendar/CalendarEvent.java
@@ -0,0 +1,42 @@
+package com.nighthawk.spring_portfolio.mvc.calendar;
+
+import java.util.Date;
+
+public class CalendarEvent {
+    private Long id;
+    private String title;
+    private String description;
+    private Date date;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
--- src/main/java/com/nighthawk/spring_portfolio/mvc/calendar/CalendarViewController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/calendar/CalendarViewController.java
@@ -1,17 +1,17 @@
 package com.nighthawk.spring_portfolio.mvc.calendar;
 
-import com.nighthawk.spring_portfolio.mvc.calendar.Event;
-import com.nighthawk.spring_portfolio.mvc.calendar.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/events")
 public class CalendarViewController {
 
     @Autowired
-    private EventService eventService;
+    private CalendarEventRepository eventService;
 
     @GetMapping("/calendar")
     public String calendar() {
@@ -20,12 +20,12 @@ public class CalendarViewController {
     }
 
     @PostMapping("/add")
-    public Event addEvent(@RequestBody Event event) {
+    public CalendarEvent addEvent(@RequestBody CalendarEvent event) {
         return eventService.saveEvent(event);
     }
 
     @GetMapping
-    public List<Event> getAllEvents() {
+    public List<CalendarEvent> getAllEvents() {
         return eventService.getAllEvents();
     }
 }
